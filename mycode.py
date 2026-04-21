9dc804c0f4a619f2fbd5d3844b7dc5cde5caedba
Sravan <georgeragan123@gmail.com>
2026-04-21 16:08:28 +0530

    4 commit

--- mycode.py
+++ mycode.py
@@ -7,9 +7,12 @@ data={
     "city":["Knr","Clt","Tvm"]
 }
 df=pd.DataFrame(data)
+
 new_row_loc={"Name":"Manu","Age":34,"city":"Tsr"}
 df.loc[len(df.index)]=new_row_loc
 
+new_row_loc2={"Name":"Aisu","Age":64,"city":"Ksd"}
+df.loc[len(df.index)]=new_row_loc2
 
 dir="data"
 os.makedirs(dir,exist_ok=True)
